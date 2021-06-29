a31fe32e9516a10b5ab189a00730775bf5b94e76
Evan Louden <evanlouden@gmail.com>
2021-06-28 22:42:42 -0400

    Improve About/Row styling

---
+++ src/data/bios/jordan.md
@@ -0,0 +1,14 @@
+---
+name: "Jordan Gravel"
+<<<<<<< HEAD
+bio: ""
+instrument: "Keys"
+image: ../../images/evan.jpg
+order: 7
+publish: true
+=======
+bio: "Also playes in the Gravel Project"
+instrument: "Bass"
+image: ../../images/evan.jpg
+>>>>>>> bfd9816 (Improve About/Row styling)
+---
--- src/pages/about.js
+++ src/pages/about.js
@@ -32,6 +32,7 @@ const About = () => {
 
   return (
     <Layout>
+<<<<<<< HEAD
       <SEO title="Carboy | About" />
       {
         biosData.allMarkdownRemark.edges.map((data, index) => {
@@ -47,6 +48,43 @@ const About = () => {
           )
         })
       }
+=======
+      <SEO title="About" />
+      <div className="max-w-screen-lg mx-auto">
+<<<<<<< HEAD
+        <p className="m-4 text-2xl text-white font-extralight">Carboy is a psychedelic funk/soul band based in Boston. Comprised of experienced musicians, they play a mix of original instrumentals and lesser known soul classics from the 60s.</p>
+        {
+          biosData.allMarkdownRemark.edges.map((data, index) => {
+            if (data.node.frontmatter.publish) {
+              return (
+                <Row
+                  key={data.node.id}
+                  index={index}
+                  bio={data.node.frontmatter.bio}
+                  name={data.node.frontmatter.name}
+                  instrument={data.node.frontmatter.instrument}
+                  fluid={data.node.frontmatter.image.childImageSharp.fluid}
+                />
+              )
+            }
+=======
+        {
+          biosData.allMarkdownRemark.edges.map((data, index) => {
+            return (
+              <Row
+                key={data.node.id}
+                index={index}
+                bio={data.node.frontmatter.bio}
+                name={data.node.frontmatter.name}
+                instrument={data.node.frontmatter.instrument}
+                fluid={data.node.frontmatter.image.childImageSharp.fluid}
+              />
+            )
+>>>>>>> bfd9816 (Improve About/Row styling)
+          })
+        }
+      </div>
+>>>>>>> f5e169d (Improve About/Row styling)
     </Layout>
   )
 }
